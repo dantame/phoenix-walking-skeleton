40fb4ec4c814813371ef5f2823419d6b084bb52c
Dan Tame <dantame@Dans-MBP.lan>
2019-10-09 19:28:00 +0100

    format code and update dockerfile to check formatting

--- test/walking_skeleton_web/views/error_view_test.exs
+++ test/walking_skeleton_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule WalkingSkeletonWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(WalkingSkeletonWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(WalkingSkeletonWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
